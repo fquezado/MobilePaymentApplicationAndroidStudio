7bcf1808c0369931777ad9a14ac2fa0f09197f57
fquezado <fquezado18@gmail.com>
2023-11-06 15:33:01 -0500

    testing branch rules

--- app/src/main/java/com/group4/mobilepaymentapplication/LoginActivity.java
+++ app/src/main/java/com/group4/mobilepaymentapplication/LoginActivity.java
@@ -31,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
         registerTextView = findViewById(R.id.registerTextView);
         userPreferences = new UserPreferences(this);
 
-        //login button chdudhfduhf dinner skdskd
+        //login button chdudhfduhf din
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
