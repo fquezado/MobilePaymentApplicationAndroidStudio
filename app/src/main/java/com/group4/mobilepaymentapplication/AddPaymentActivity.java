08a007f02944a64f7bde4537473bb7b048b679dc
fquezado <fquezado18@gmail.com>
2023-11-09 19:36:05 -0500

    Adding all pages for application!

---
+++ app/src/main/java/com/group4/mobilepaymentapplication/AddPaymentActivity.java
@@ -0,0 +1,16 @@
+package com.group4.mobilepaymentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPaymentActivity extends AppCompatActivity {
+
+
+    //Use case for M03-A05 One Implemented Use Case!
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_payment);
+    }
+}
