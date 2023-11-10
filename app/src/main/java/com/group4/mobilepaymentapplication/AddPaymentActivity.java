d522c40bc6157bc50e946e0880716a1fff74d01d
fquezado <fquezado18@gmail.com>
2023-11-09 19:37:13 -0500

    adding pages

--- app/src/main/java/com/group4/mobilepaymentapplication/AddPaymentActivity.java
+++ app/src/main/java/com/group4/mobilepaymentapplication/AddPaymentActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class AddPaymentActivity extends AppCompatActivity {
 
 
-    //Use case for M03-A05 One Implemented Use Case!
+    //Use case for M03-A05 One Implemented Use Case! CODE UNDER HERE
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
