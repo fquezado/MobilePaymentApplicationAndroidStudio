138aac1a0b66cd44b7bd1a3af7368171c5667e86
fquezado <fquezado18@gmail.com>
2023-12-04 16:31:36 -0500

    fixing code

--- app/src/main/java/com/group4/mobilepaymentapplication/AccountInformation.java
+++ app/src/main/java/com/group4/mobilepaymentapplication/AccountInformation.java
@@ -14,6 +14,9 @@ public class AccountInformation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_information);
         Button homeButton = findViewById(R.id.home_button);
