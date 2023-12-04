b7c6a53113a1aa2069808cfe2416277593622022
fquezado <fquezado18@gmail.com>
2023-12-03 21:37:20 -0500

    implemented pay method and completely revamped payment preferences

--- app/src/main/java/com/group4/mobilepaymentapplication/RecyclerAdapter.java
+++ app/src/main/java/com/group4/mobilepaymentapplication/RecyclerAdapter.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
 
-    private ArrayList<PaymentCard> cardList;
+    private ArrayList<Object> itemList;
     private OnItemClickListener mListener;
 
     // Interface for click events
@@ -26,19 +26,19 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     }
 
     // Constructor
-    public RecyclerAdapter(ArrayList<PaymentCard> cardList) {
-        this.cardList = cardList;
+    public RecyclerAdapter(ArrayList<Object> itemList) {
+        this.itemList = itemList;
     }
 
     // ViewHolder class
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        private TextView nameTxt;
-        private TextView numberTxt;
+        private TextView primaryText;
+        private TextView secondaryText;
 
         public MyViewHolder(final View view) {
             super(view);
-            nameTxt = view.findViewById(R.id.textCardHolderName);
-            numberTxt = view.findViewById(R.id.textCardNumber);
+            primaryText = view.findViewById(R.id.textPrimary);
+            secondaryText = view.findViewById(R.id.textSecondary);
 
             // Set the click listener for the itemView
             view.setOnClickListener(new View.OnClickListener() {
@@ -64,14 +64,21 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerAdapter.MyViewHolder holder, int position) {
-        String number = "Card Number: " + cardList.get(position).getCardNumber();
-        String name = "Card Name: " + cardList.get(position).getName();
-        holder.numberTxt.setText(number);
-        holder.nameTxt.setText(name);
+        Object item = itemList.get(position);
+        if (item instanceof CreditCard) {
+            CreditCard card = (CreditCard) item;
+            holder.primaryText.setText("Card Number: " + card.getCardNumber());
+            holder.secondaryText.setText("Card Name: " + card.getName());
+
+        } else if (item instanceof BankAccount) {
+            BankAccount account = (BankAccount) item;
+            holder.primaryText.setText("Account Holder: " + account.getAccountHolderName());
+            holder.secondaryText.setText("Account Number: " + account.getAccountNumber());
+        }
     }
 
     @Override
     public int getItemCount() {
-        return cardList.size();
+        return itemList.size();
     }
 }
