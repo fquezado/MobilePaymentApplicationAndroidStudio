5c3a7b095aac2e157c01b893b797eb18c8bb0a57
fquezado <fquezado18@gmail.com>
2023-12-06 18:11:12 -0500

    fixing code

--- app/src/main/java/com/group4/mobilepaymentapplication/AccountInformation.java
+++ app/src/main/java/com/group4/mobilepaymentapplication/AccountInformation.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AccountInformation extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -31,4 +30,5 @@ public class AccountInformation extends AppCompatActivity {
             }
         });
     }
+
 }
